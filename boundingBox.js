16baf15731b8ceb4c7bdd9a8eaccc5844297a8c2
saltcam <saltcam@uw.edu>
2024-01-17 20:57:09 -0800

    Sanity commit, bounding box exists for player, working on making an enemy that exists in the world.

---
+++ boundingBox.js
@@ -0,0 +1,50 @@
+class BoundingBox {
+    /**
+     * @param x the top left of the box
+     * @param y the top of the box
+     * @param width the distance from the left to right sides of box
+     * @param height the distance from top to bottom of box
+     * @param type 'player': take damage when colliding with 'enemy' or 'enemyAttack'.
+     *             'enemy': take damage when colliding with 'playerAttack'.
+     *             'enemyAttack': boxes labeled 'player' take damage upon collision.
+     *             'playerAttack': boxes labeled 'enemy' take damage upon collision.
+     */
+    //TODO is there a better way to handle box types?
+    constructor(x, y, width, height, type) {
+        this.left = x;
+        this.top = y;
+        this.width = width;
+        this.height = height;
+        this.type = type;
+    }
+
+    // update the x and y values in case the entity moves
+    update(newX, newY){
+        this.x = newX;
+        this.y = newY;
+    }
+
+    draw(ctx, game) {
+
+        // draws the box for you to see
+        ctx.beginPath();
+        switch (this.type){
+            case "player":
+                ctx.strokeStyle = 'Blue';
+                break;
+            case "enemy":
+                ctx.strokeStyle = 'Red';
+                break;
+            default: ctx.strokeStyle = 'White';
+            break;
+        }
+
+            ctx.strokeRect(
+                this.left, this.top,
+                this.width,
+                this.height);
+            ctx.closePath();
+
+    }
+
+}
--- zombie.js
+++ zombie.js
@@ -1,29 +1,31 @@
-// Dude is our main character. He can move up down left and right on the map.
-class Dude{
+class Zombie {
     constructor(game) {
         this.game = game;
 
-        // Define the animator for the character. The parameters are: spritesheet, xStart, yStart, width, height, frameCount, frameDuration
-        // For unarmed walk, use width of 48, height of 55, frameCount of 4, and frameDuration of 0.2
-        // For scythe walk, use width of 92, height of 55, frameCount of 4, and frameDuration of 0.2
-        // For unarmed standing, use width of 48, height of 55, frameCount of 2, and frameDuration of 0.5
-        // For scythe standing, use width of 92, height of 55, frameCount of 2, and frameDuration of 0.5
-        this.animator = new Animator(ASSET_MANAGER.getAsset("./sprites/dude-spritesheet-stand-scythe.png"), 0, 0, 92, 55, 2, 0.5);
+        this.animator = new Animator(ASSET_MANAGER.getAsset("./sprites/zombie-spritesheet.png"), 0, 0, 48, 55, 2, 0.5);
         this.yOffset = -25; // Offsets the character upwards from the center of the canvas (you see this used in the draw() method below)
 
         // Calculate the middle of the canvas, then adjust by half of the character's width and height to center the character
-        this.x = (game.ctx.canvas.width / 2) - (this.animator.width * 1.5 / 2); 
+        this.x = (game.ctx.canvas.width / 2) - (this.animator.width * 1.5 / 2);
         this.y = (game.ctx.canvas.height / 2) - (this.animator.height * 1.5 / 2);
 
         // initializing the player's bounding box
-        //TODO find a better way of getting height and width values for player
-        this.box = new BoundingBox(this.x + 40, this.y - 20, 57, 85, "player");
+        this.box = new BoundingBox(this.x + 40, this.y - 20, 57, 85, "enemy");
 
         this.movementSpeed = 200; // Movement Speed
         this.lastMove = "right"; // Default direction
         this.isMoving = false;  // Is the character currently moving?
         this.currentAnimation = "standing"; // Starts as "standing" and changes to "walking" when the character moves
-    };
+    }
+
+    /**
+     * calculates which angles to move at in order to chase player
+     */
+    calcTargetAngle() {
+
+
+
+    }
 
     update() {
         // Calculate the delta time which is defined as the time passed in seconds since the last frame.
@@ -32,32 +34,34 @@ class Dude{
 
         this.isMoving = false; // Reset the isMoving flag to false
 
-        // Initialize movement vector components, we will use this to normalize the movement vector (so diagonal movement isn't faster than horizontal or vertical movement)
-        let moveX = 0;
-        let moveY = 0;
 
-        // Update movement vector based on key presses
-        if (this.game.keys["w"]) moveY -= 1;
-        if (this.game.keys["s"]) moveY += 1;
-        if (this.game.keys["a"]) {
-            moveX -= 1;
-            this.lastMove = "left";     // Remember the last direction the character moved
-        }
-        if (this.game.keys["d"]) {
-            moveX += 1;
-            this.lastMove = "right";    // Remember the last direction the character moved
-        }
 
-        // Check if the character is moving
-        this.isMoving = (moveX !== 0 || moveY !== 0);
-
-        // Normalize the movement vector by calculating the length of the vector and dividing the components by the length
-        // If this confuses you, just know that all this is doing is preventing diagonal movement from being faster than horizontal or vertical movement
-        let length = Math.sqrt(moveX * moveX + moveY * moveY);
-        if (length > 0) {
-            moveX /= length;
-            moveY /= length;
-        }
+        // // Initialize movement vector components, we will use this to normalize the movement vector (so diagonal movement isn't faster than horizontal or vertical movement)
+        // let moveX = 0;
+        // let moveY = 0;
+        //
+        // // Update movement vector based on key presses
+        // if (this.game.keys["w"]) moveY -= 1;
+        // if (this.game.keys["s"]) moveY += 1;
+        // if (this.game.keys["a"]) {
+        //     moveX -= 1;
+        //     this.lastMove = "left";     // Remember the last direction the character moved
+        // }
+        // if (this.game.keys["d"]) {
+        //     moveX += 1;
+        //     this.lastMove = "right";    // Remember the last direction the character moved
+        // }
+        //
+        // // Check if the character is moving
+        // this.isMoving = (moveX !== 0 || moveY !== 0);
+        //
+        // // Normalize the movement vector by calculating the length of the vector and dividing the components by the length
+        // // If this confuses you, just know that all this is doing is preventing diagonal movement from being faster than horizontal or vertical movement
+        // let length = Math.sqrt(moveX * moveX + moveY * moveY);
+        // if (length > 0) {
+        //     moveX /= length;
+        //     moveY /= length;
+        // }
 
         // Apply movement to the character's world position in the game engine
         this.game.worldX += moveX * delta;
@@ -82,4 +86,5 @@ class Dude{
             this.lastMove); // Pass the lastMove as direction
         this.box.draw(ctx, this.game);
     }
+
 }
