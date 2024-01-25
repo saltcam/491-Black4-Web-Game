b9b809dd36d62aa7e92d9ce17332f64efc9b96be
saltcam <saltcam@uw.edu>
2024-01-24 19:10:54 -0800

    New attack circle system, collision works but crashes game when an entity is added. Background rework WIP,win condition pseudo implemented. New enemy, new player design.

---
+++ portal.js
@@ -0,0 +1,17 @@
+class Portal extends Entity {
+    constructor(game, worldX, worldY) {
+        super(1, 1, 0, game, worldX, worldY, 24, 58, "portal", 0,
+            "./sprites/portal.png",
+            1, 0, 24, 58, 4, 0.18, 2.5);
+    }
+
+    draw(ctx) {
+        let screenX = this.worldX - this.game.camera.x;
+        let screenY = this.worldY - this.game.camera.y;
+
+        // Draw the player at the calculated screen position
+        this.animator.drawFrame(this.game.clockTick, ctx, screenX, screenY, this.lastMove);
+        this.boundingBox.draw(ctx, this.game);
+        this.drawHealth(ctx);
+    }
+}
