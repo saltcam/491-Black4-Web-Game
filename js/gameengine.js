aa825a19399495d8cdebdd8b1512e721fcc658c6
Nickolas Zahos <nzahos@yahoo.com>
2024-02-22 17:15:05 -0800

    - Removed debug portal.

--- js/gameengine.js
+++ js/gameengine.js
@@ -197,7 +197,7 @@ class GameEngine {
         this.spawnUpgradeChest(1797, 2802);
 
         // Debug Portal
-        this.spawnPortal(0, 100, 0);
+        //this.spawnPortal(0, 100, 0);
 
         // Rocks
         let newEntity = this.addEntity(new Map_object(this, -250, 0, 55, 56-30, "./sprites/map_rock_object.png", 0, 0, 86, 56, 1, 1, 2));
